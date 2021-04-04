1dabd9154f236094673ab404f2639061fffad1e7
Alex Bryant <alexanderpbryant@gmail.com>
2021-04-04 18:26:01 -0500

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+function book (title, author, pages, read) {
+    this.title = titlel;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function (){
+      return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
+    }
+  }
+  
+  let myLibrary = [];
+
+  function Book() {
+    // the constructor...
+  }
+  
+  function addBookToLibrary() {
+    // do stuff here
+  }  
