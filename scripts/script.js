cae8b15467999c9422e46c08916163c689b4cf77
Alex Bryant <alexanderpbryant@gmail.com>
2021-04-07 09:13:04 -0500

    adding books and pushing card info to page is working, no formatting yet

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,37 @@
-function book (title, author, pages, read) {
-    this.title = titlel;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function (){
-      return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
-    }
-  }
+let myLibrary = [];
   
-  let myLibrary = [];
+function Book (title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  //this.info = function (){
+  //  return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
+  //}
+}
+  
+function addBookToLibrary() {
+  while (libraryFeed.firstChild)  {
+    libraryFeed.removeChild(libraryFeed.lastChild);
+  }
+  var titleField = document.getElementById('titleField').value;
+  var authorField = document.getElementById('authorField').value;
+  var pagesField = document.getElementById('pagesField').value;
+  var readField = document.getElementById('readField').value;
+  var book = new Book(titleField, authorField, pagesField, readField);
+  myLibrary.push(book);
 
-  function Book() {
-    // the constructor...
-  }
+  const chronologicalFeed = myLibrary.map(function (bookListing){
+    var bookCard = document.createElement("div");
+    bookCard.classList.add("bookCard");
+    bookCard.textContent = `${bookListing.title} ${bookListing.author} ${bookListing.pages} ${bookListing.read}`
+    libraryFeed.appendChild(bookCard);
+  })
   
-  function addBookToLibrary() {
-    // do stuff here
-  }  
+}  
+
+const libraryFeed = document.querySelector('.libraryFeed');
+const addBookButton = document.querySelector('.addBookButton');
+const addBookSubmit = document.getElementById('addBookSubmit');
+
+addBookSubmit.addEventListener('click', addBookToLibrary);
